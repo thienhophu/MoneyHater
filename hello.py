253c04e23b8c1a4bb336d024225a12e6613e7d48
Thien Ho <thien.hophu@gmail.com>
2022-06-18 00:39:23 +0700

    adding basic steps for learning

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Install Python 3
+# Run cmd python3 hello.py
+# Try to edit things in this file and re-run again to see the changes
+
+print("hello")
+
+if 5 < 2:
+    print("Greater")
+else:
+    print("Smaller")
+
+exit()
